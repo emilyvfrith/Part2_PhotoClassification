bc127426326745a0a89e8655d14d78c86a701518
Emily Frith <e.v.frith@newcastle.ac.uk>
2021-05-21 11:37:31 +0100

    Checking version control

---
+++ Photo Script.R
@@ -0,0 +1,11 @@
+## BIO8068 Assignment Part 2 Photo Classification
+## Emily Frith 05/05/2021
+## flowering plants
+## foxgloves
+## Genus Digitalis
+## selected flowers
+## Digitalis purpurea
+## Digitalis lutea
+## Digitalis thapsi
+## Digitalis viridiflora 
+
